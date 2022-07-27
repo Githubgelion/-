3a302f62b9a9eaa418ab0c6fd47fe242e8c7d8ec
Andrey <45911635+Githubgelion@users.noreply.github.com>
2022-07-27 15:13:46 +0300

    Первое тестовое задание

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    public static int Main()
+    {
+        int j = 1; // initial start number 
+        for (int i = 1; i <= 1000; i += j) // cycle for displaying fibonacci numbers
+        {
+            Console.WriteLine(i); 
+            j = i - j; 
+        }
+        Console.ReadKey();
+        return 0;
+    }
+}
